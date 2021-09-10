175c709f884dd1461acc221387e9aa71b18a70e0
mattkjames7 <mattkjames7@gmail.com>
2021-09-10 12:50:21 +0100

    figured out a way of storing dtypes

--- RecarrayTools/__init__.py
+++ RecarrayTools/__init__.py
@@ -6,3 +6,6 @@ from .ReduceRecarray import ReduceRecarray
 from .InterpRecarrayFields import InterpRecarrayFields
 from .SaveRecarray import SaveRecarray
 from .AppendFields import AppendFields
+
+from ._StoreDtype import _StoreDtype
+from ._ReadDtype import _ReadDtype
